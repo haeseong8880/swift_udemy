7f292cae6a5c40f6c78c9976eb6bec2efc582459
Haeseong <hesung0805@gmail.com>
2022-02-17 16:17:31 +0900

    2

---
+++ I am Rich/I am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by haeseongJung on 2022/02/14.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
