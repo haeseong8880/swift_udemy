fe564ea1ee04721305676f1ad6df487fc2d78c86
Haeseong <hesung0805@gmail.com>
2022-03-08 11:40:25 +0900

    1

---
+++ Flash-Chat-iOS13/Flash Chat iOS13/Models/Message.swift
@@ -0,0 +1,14 @@
+//
+//  Message.swift
+//  Flash Chat iOS13
+//
+//  Created by haeseongJung on 2022/03/07.
+//  Copyright © 2022 Angela Yu. All rights reserved.
+//
+
+import Foundation
+
+struct Message {
+    let sender: String
+    let body: String
+}
---
+++ Flash-Chat-iOS13/Flash Chat iOS13/Views/MessageCell.swift
@@ -0,0 +1,30 @@
+//
+//  MessageCell.swift
+//  Flash Chat iOS13
+//
+//  Created by haeseongJung on 2022/03/07.
+//  Copyright © 2022 Angela Yu. All rights reserved.
+//
+
+import UIKit
+
+class MessageCell: UITableViewCell {
+
+    @IBOutlet weak var messageBubble: UIView!
+    @IBOutlet weak var lebel: UILabel!
+    
+    @IBOutlet weak var rightImageView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        messageBubble.layer.cornerRadius = messageBubble.frame.size.height / 5
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
